d8c37cdaf0fd36a1034129b9dc3a3ef70e308fcb
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-02-28 23:55:20 +0800

    remove  feature, and add #[derive(Copy, Clone)]

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -2,11 +2,13 @@ use crate::config::*;
 use crate::task::TaskContext;
 use crate::trap::TrapContext;
 
+#[derive(Copy, Clone)]
 #[repr(align(4096))]
 struct KernelStack {
     data: [u8; KERNEL_STACK_SIZE],
 }
 
+#[derive(Copy, Clone)]
 #[repr(align(4096))]
 struct UserStack {
     data: [u8; USER_STACK_SIZE],
