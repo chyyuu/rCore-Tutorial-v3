c97e5cd90e16886a9767e322756b21e17ba05b58
chyyuu <chyyuu@users.noreply.github.com>
2026-01-07 13:49:24 +0000

    deploy: 8aee335ed274691d87f89e512e8abc08e3536adf

---
+++ ch5/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["os",["",[["boards",[],["qemu.rs"]],["mm",[],["address.rs","frame_allocator.rs","heap_allocator.rs","memory_set.rs","mod.rs","page_table.rs"]],["sync",[],["mod.rs","up.rs"]],["syscall",[],["fs.rs","mod.rs","process.rs"]],["task",[],["context.rs","manager.rs","mod.rs","pid.rs","processor.rs","switch.rs","task.rs"]],["trap",[],["context.rs","mod.rs"]]],["config.rs","console.rs","lang_items.rs","loader.rs","logging.rs","main.rs","sbi.rs","timer.rs"]]]]');
+//{"start":19,"fragment_lengths":[453]}
