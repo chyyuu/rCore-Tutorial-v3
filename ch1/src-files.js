c9a9957984802fbb3ef47856704ceaf621d3174f
chyyuu <chyyuu@users.noreply.github.com>
2026-01-07 09:59:16 +0000

    deploy: b8b5426d45af5b1e3fe6188e7bbc75ba03aa7db5

--- ch1/src-files.js
+++ ch1/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["os",["",[],["console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[77]}
+createSrcSidebar('[["os",["",[],["console.rs","lang_items.rs","logging.rs","main.rs","sbi.rs"]]]]');
+//{"start":19,"fragment_lengths":[77]}
